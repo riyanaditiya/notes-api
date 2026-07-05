d71d893194ec69b4f34ecd8bea21bda86a536607
riyanaditiya <riyanaditiyasaputra@gmail.com>
2026-07-05 11:05:46 +0700

    feat: make NoteController, StoreNoteRequest, UpdateNoteRequest

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +19,5 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
 
     // Notes CRUD
-    // Route::apiResource('notes', NoteController::class);
+    Route::apiResource('notes', NoteController::class);
 });
